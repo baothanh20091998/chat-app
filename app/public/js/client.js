8a7e8210a65da1e2168e47553b5aceb644de6382
baothanh <hlb.thanh20091998@gmail.com>
2021-10-12 23:03:16 +0700

    chat-app

---
+++ app/public/js/client.js
@@ -0,0 +1,85 @@
+// gửi một yêu cầu kết nối tới server
+const socket = io();
+
+const acknowlegment = (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("gui tin nhan thanh cong");
+};
+
+document.getElementById("form-messages").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = document.getElementById("input-messages").value;
+  socket.emit("send-messages-client-to-server", message, acknowlegment);
+});
+
+socket.on("send-message-server-to-client", ({ username, time, content }) => {
+  document.getElementById("messages-list").innerHTML += `
+  <div class="message-item">
+  <div class="message__row1">
+    <p class="message__name">${username}</p>
+    <p class="message__date">${time}</p>
+  </div>
+  <div class="message__row2">
+    <p class="message__content">
+     ${content}
+    </p>
+  </div>
+</div>
+
+  `;
+});
+
+document.getElementById("btn-share-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Trinh duyet khong cung cap location");
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    const location = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    socket.emit("send-loaction-client-to-server", location);
+  });
+});
+
+//xử lý query string
+/**
+ * {
+ *    room: room,
+ *    username: username,
+ * }
+ */
+
+const { room, username } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+socket.emit("join-room", { room, username });
+
+socket.on("send-user-list", function (userList) {
+  document.getElementById("user-list").innerHTML = userList
+    .map(
+      (user) => `
+  <li class="app__item-user">${user.username}</li>
+  `
+    )
+    .reduce((stringHtml, stringLi) => (stringHtml += stringLi), "");
+});
+
+socket.on("send-loaction-server-to-client", ({ username, time, content }) => {
+  document.getElementById("messages-list").innerHTML += `
+  <div class="message-item">
+  <div class="message__row1">
+    <p class="message__name">${username}</p>
+    <p class="message__date">${time}</p>
+  </div>
+  <div class="message__row2">
+    <a href=${content} target="_blank" class="message__content">
+     Vi tri cua ${username}
+    </a>
+  </div>
+</div>
+  `;
+});
---
+++ app/src/server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+const app = express();
+const dateFormat = require("date-format");
+const Filter = require("bad-words");
+const {
+  addUser,
+  getListUserByRoom,
+  removeUser,
+} = require("../public/models/users");
+
+// static file
+const publicPathDirectory = path.join(__dirname, "../public");
+app.use(express.static(publicPathDirectory));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+// nhận sự kiện kết nối từ client
+io.on("connection", (socket) => {
+  //Xu ly room
+  socket.on("join-room", function ({ username, room }) {
+    socket.join(room);
+
+    const newUser = {
+      room,
+      username,
+      id: socket.id,
+    };
+    addUser(newUser);
+    io.emit("send-user-list", getListUserByRoom(room));
+    socket.emit("send-message-server-to-client", {
+      username: "ADMIN",
+      time: dateFormat("dd/mm/yyyy hh:MM", new Date()),
+      content: `Chao mung ${username} da vao ${room}`,
+    });
+
+    socket.broadcast.to(room).emit("send-message-server-to-client", {
+      username: "ADMIN",
+      time: dateFormat("dd/mm/yyyy hh:MM", new Date()),
+      content: `${username} vua tham gia vao ${room}`,
+    });
+
+    // xử lý chat
+    // nhận message từ client
+    socket.on("send-messages-client-to-server", function (message, cb) {
+      console.log("message : ", message);
+      const filter = new Filter();
+
+      if (filter.isProfane(message)) {
+        return cb("Nội dung không hợp lệ");
+      }
+      const newMessage = {
+        username,
+        time: dateFormat("dd/mm/yyyy hh:MM", new Date()),
+        content: message,
+      };
+
+      //gửi message về client
+      io.to(room).emit("send-message-server-to-client", newMessage);
+
+      cb();
+    });
+
+    socket.on("send-loaction-client-to-server", ({ latitude, longitude }) => {
+      const urlLocation = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+      const newLocation = {
+        username,
+        time: dateFormat("dd/mm/yyyy hh:MM", new Date()),
+        content: urlLocation,
+      };
+
+      //gửi message về client
+      io.to(room).emit("send-loaction-server-to-client", newLocation);
+    });
+  });
+
+  // ngắt kết nối
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log(`client ${socket.id} disconnect `);
+  });
+});
+
+const port = process.env.PORT || 3333;
+httpServer.listen(port, () => {
+  console.log("app run on port " + port);
+});
